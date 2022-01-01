5416ddd9dd1b17444536fa5ad8eef3baf8ea71c6
Dimas_Bagas_Saputro <dimazzbagazz@yahoo.co.id>
2022-01-01 10:23:30 +0700

    Add Test Dislike

--- test.py
+++ test.py
@@ -57,6 +57,30 @@ class TestWebsite(unittest.TestCase):
         # driver.find_element_by_tag_name("tp-yt-paper-item").click()
         # time.sleep(5)
 
+    def testDislike(self):
+        driver = self.driver
+        driver.get("https://www.youtube.com")
+
+        driver.find_element_by_link_text("SIGN IN").click()
+        time.sleep(2)
+        driver.find_element_by_name("identifier").send_keys("tt1974036@gmail.com")
+        time.sleep(5)
+        driver.find_element_by_css_selector("span.VfPpkd-vQzf8d").click()
+        time.sleep(5)
+        driver.find_element_by_name("password").send_keys("IniTest12")
+        time.sleep(5)
+        driver.find_element_by_css_selector("span.VfPpkd-vQzf8d").click()
+        time.sleep(15)
+        driver.find_element_by_name("search_query").send_keys("lofi")
+        time.sleep(5)
+        driver.find_element_by_id("search-icon-legacy").click()
+        time.sleep(5)
+        driver.find_element_by_link_text("lofi hip hop radio - beats to relax/study to").click()
+        time.sleep(15)
+        driver.find_element_by_link_text("DISLIKE").click()
+        time.sleep(15)
+
+
     def tearDown(self):
         self.driver.close()
 
